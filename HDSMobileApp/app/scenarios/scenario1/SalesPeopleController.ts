df122797336b86e9fbee780f8f20f6d6251dcca2
unknown <JM054913@H1525779.hsi.hughessupply.com>
2015-09-08 15:55:26 -0400

    added sales people table

---
+++ HDSMobileApp/app/scenarios/scenario1/SalesPeopleController.ts
@@ -0,0 +1,85 @@
+﻿"use strict";
+import Services = require("../../services/Services");
+import Data = require("../../modules/Data");
+
+class SalesPeopleController implements WidgetView<any> {
+
+    public initView(appTools: Main, ngApp: ng.IModule) {
+        // NOTE: DO NOT 'angular.module(...)', use the 'ngApp' parameter above
+        this.setupSalesPeopleDirective(ngApp);
+    }
+
+
+    public setupSalesPeopleDirective(ngApp: ng.IModule) {
+
+        // define a directive and now we can use products in the html
+        ngApp.directive("salespeopleTable", function () {
+            return {
+                // E is for element we are defining our own element
+                // A is for attribute if you were to use directive as an attribute
+                restrict: "E",
+                //point to html file
+                templateUrl: "/app/scenarios/scenario1/salespeople-table.html",
+                //add in a controller
+                controller: ["$scope", "$http", function ($scope, $http) {
+                    
+                    //get all the territories and set them as an instance variable for the controller
+                    this.salespeople = Data.getSalesPersons();
+                    console.log(this.salespeople);
+
+                    // set an initial value to sort by
+                    $scope.predicate = 'customerID';
+                    // set an initial reverse value
+                    // false is ascending true is decending
+                    // Made a decision to start by decending because the data looked nicer that way on the table
+                    $scope.reverse = false;
+
+                    $scope.order = function (predicate) {
+                        // if the same header is clicked on again reverse the sort boolean and set the current predicate
+                        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : true;
+                        $scope.predicate = predicate;
+                    };
+
+                    //reset the searchTerm and refocus to the search box
+                    $scope.inputClear = function () {
+                        $scope.searchTerm = "";
+                        jQuery('.salesSearch').focus();
+                    };
+
+                    //this function is called when a user clicks on a table row
+                    //the product the user clicked on is passed in as product
+                    $scope.showTerritory = function (territory) {
+                        //var tempObj = {};
+                        //jQuery.extend(tempObj, territory);
+                        //jQuery.extend(tempObj, Data.getSalesPeopleByTerritoryId(territory.territoryId));
+                        //jQuery.extend(tempObj, tempObj.businessEntityId);
+                        var salesPeople = Data.getSalesPeopleByTerritoryId(territory.territoryId);
+                        var employees = Data.getEmployees();
+
+                        for (var i = 0; i < salesPeople.length; i++) {
+
+                            for (var j = 0; j < employees.length; j++) {
+
+                                if (salesPeople[i].businessEntityId == employees[j].businessEntityId) {
+                                    jQuery.extend(salesPeople[i], employees[j]);
+                                }
+                            }
+                        }
+                        console.log(salesPeople);
+                        $scope.terrSalesPeople = salesPeople;
+                        $scope.territory = territory;
+                    };
+                }],
+                // add an alias for a controller
+                controllerAs: "SalesPeopleCtrl"
+            };
+        });
+    }
+
+    public deregister(appTools: Main, view: SalesPeopleController) {
+
+    }
+
+}
+
+export = SalesPeopleController;
